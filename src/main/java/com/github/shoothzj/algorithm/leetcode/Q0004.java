fd5df7ef05dadc0c58126d749a27e771cfd6d3d0
ShootHzj <shoothzj@gmail.com>
2019-02-14 01:14:52 +0800

    Initial commit

---
+++ src/main/java/com/github/shoothzj/algorithm/leetcode/Q0004.java
@@ -0,0 +1,37 @@
+package com.github.shoothzj.algorithm.leetcode;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Q0004 {
+
+    private static final Logger log = LoggerFactory.getLogger(Q0004.class);
+
+    /**
+     * There are two sorted arrays nums1 and nums2 of size m and n respectively.
+     *
+     * Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+     *
+     * You may assume nums1 and nums2 cannot be both empty.
+     *
+     *
+     *
+     * Example 1:
+     *
+     * nums1 = [1, 3]
+     * nums2 = [2]
+     *
+     * The median is 2.0
+     * Example 2:
+     *
+     * nums1 = [1, 2]
+     * nums2 = [3, 4]
+     *
+     * The median is (2 + 3)/2 = 2.5
+     */
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        //todo
+        return 0;
+    }
+
+}
---
+++ src/main/java/com/github/shoothzj/algorithm/leetcode/Q0007.java
@@ -0,0 +1,51 @@
+package com.github.shoothzj.algorithm.leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Q0007 {
+
+    public int reverse(int x) {
+        long aux = 0;
+        while (x != 0) {
+            int temp = x % 10;
+            aux = aux * 10 + temp;
+            x = x / 10;
+        }
+        if (aux < Integer.MAX_VALUE && aux > Integer.MIN_VALUE) {
+            return (int) aux;
+        }
+        return 0;
+    }
+
+    public int reverseAnswer(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+
+    @Test
+    public void test01() {
+        int reverse = reverse(-123);
+        Assert.assertEquals(-321, reverse);
+    }
+
+    @Test
+    public void test02() {
+        int reverse = reverse(123);
+        Assert.assertEquals(321, reverse);
+    }
+
+    @Test
+    public void test03() {
+        int reverse = reverse(1200);
+        Assert.assertEquals(21, reverse);
+    }
+
+}
